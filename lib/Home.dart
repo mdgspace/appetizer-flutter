cda9a27e00de7e0641e3b253660cc6bd69effa80
Nitish Aggarwal <royalnitish21@gmail.com>
2019-06-06 10:13:49 +0530

    feat: Login is working and use of GestureDetector instead of flat buttons in login.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,8 +3,6 @@ import "colors.dart";
 import 'HorizontalDatePicker.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show CalendarCarousel;
 
-void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
