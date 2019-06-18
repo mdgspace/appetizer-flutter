b68a89f3da634fe8ce1ad56918fd4885b93f138c
Nitish Aggarwal <royalnitish21@gmail.com>
2019-06-18 17:08:03 +0530

    feat: Add RebateHistory Implementation

--- lib/screens/rebate_history/rebate_history_screen.dart
+++ lib/screens/rebate_history/rebate_history_screen.dart
@@ -1,9 +1,28 @@
+import 'package:appetizer/models/transaction/yearlyRebate.dart';
+import 'package:appetizer/services/transaction.dart';
 import 'package:flutter/material.dart';
 
-import 'rebate_dropdown_filter.dart';
+import '../../login.dart';
 import 'rebate_history_card.dart';
 
-class RebateHistoryScreen extends StatelessWidget {
+class RebateHistoryScreen extends StatefulWidget {
+
+  @override
+  _RebateHistoryScreenState createState() => _RebateHistoryScreenState();
+}
+
+class _RebateHistoryScreenState extends State<RebateHistoryScreen> {
+
+  final _yearList = [
+    DateTime.now().year,
+    DateTime.now().year - 1,
+    DateTime.now().year - 2,
+    DateTime.now().year - 3,
+    DateTime.now().year - 4,
+  ];
+
+  int currentItemSelected = DateTime.now().year;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +41,7 @@ class RebateHistoryScreen extends StatelessWidget {
         children: <Widget>[
           Container(
             height: 150.0,
-            child: RebateDropdownFilter(),
+            child: getDropdownFilter(),
           ),
           Expanded(
             child: ListView(
@@ -39,5 +58,90 @@ class RebateHistoryScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget getDropdownFilter(){
+    return Container(
+      color: const Color.fromRGBO(121, 85, 72, 1),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 4.0),
+                child: Text(
+                  'Filter by',
+                  style: TextStyle(
+                    fontSize: 16.5,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 2.0),
+                child: Text(
+                  'Year',
+                  style: TextStyle(
+                      color: const Color.fromRGBO(0, 0, 0, 0.54),
+                      fontSize: 16.5),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(16.0, 2.0, 16.0, 8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color:
+                                    const Color.fromRGBO(0, 0, 0, 0.15)))),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<int>(
+                            value: currentItemSelected,
+                            items: _yearList.map((int dropDownItem) {
+                              return DropdownMenuItem<int>(
+                                value: dropDownItem,
+                                child: Text(
+                                  '$dropDownItem',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (int newValueSelected) {
+                              _onDropDownItemSelected(newValueSelected);
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onDropDownItemSelected(int newValueSelected) {
+    setState(() {
+      currentItemSelected = newValueSelected;
+    });
+  }
+
+  YearlyRebate getYearlyData(int currentItemSelected) {
+    getUserDetails().then((userDetails){
+      getYearlyRebate(userDetails.getString("token"), currentItemSelected).then((yearlyRebate){
+        return yearlyRebate;
+      });
+    });
+    return null;
   }
 }
