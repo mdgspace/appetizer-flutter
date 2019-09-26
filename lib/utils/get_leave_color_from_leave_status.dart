811cc7e86deae8f95100c685f908be2383426db9
Nitish Aggarwal <royalnitish21@gmail.com>
2019-09-26 19:54:29 +0530

    feat: Add getColourForLeave Util

---
+++ lib/utils/get_leave_color_from_leave_status.dart
@@ -0,0 +1,24 @@
+import 'package:appetizer/models/menu/week.dart';
+import 'package:flutter/material.dart';
+
+Color getLeaveColorFromLeaveStatus(LeaveStatus ls) {
+  switch (ls) {
+    case LeaveStatus.N:
+      return Colors.white;
+      break;
+    case LeaveStatus.A:
+      return Colors.green;
+      break;
+    case LeaveStatus.D:
+      return Colors.red;
+      break;
+    case LeaveStatus.P:
+      return Colors.yellow;
+      break;
+    case LeaveStatus.U:
+      return Colors.blue;
+      break;
+    default:
+      return Colors.white;
+  }
+}
