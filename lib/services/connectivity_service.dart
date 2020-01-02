299a95f04d16f3d3b24ccf92ae330a69f3392b7f
Nitish Aggarwal <royalnitish21@gmail.com>
2020-01-02 19:22:49 +0530

    feat: Adds Connectivity Service to return stream of connection status

---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:appetizer/enums/connectivity_status.dart';
+import 'package:connectivity/connectivity.dart';
+
+class ConnectivityService {
+  StreamController<ConnectivityStatus> connectionStatusController =
+      StreamController<ConnectivityStatus>();
+
+  ConnectivityService() {
+    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      //convert this result to our enum
+      var connectionStatus = _getStatusFromResult(result);
+      connectionStatusController.add(connectionStatus);
+    });
+  }
+
+  ConnectivityStatus _getStatusFromResult(ConnectivityResult result) {
+    switch (result) {
+      case ConnectivityResult.mobile:
+        return ConnectivityStatus.Cellular;
+      case ConnectivityResult.wifi:
+        return ConnectivityStatus.Wifi;
+      case ConnectivityResult.none:
+        return ConnectivityStatus.Offline;
+      default:
+        return ConnectivityStatus.Offline;
+    }
+  }
+}
