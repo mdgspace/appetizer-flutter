652f247eae14fee1a3e4d20e6f67b4f006613b40
am15h <amishgarg123@gmail.com>
2020-01-30 23:46:01 +0530

    feat: (ChangeNotifier) UserDetailsModel

---
+++ lib/change_notifiers/user_details_model.dart
@@ -0,0 +1,47 @@
+import 'package:appetizer/ui/login/login.dart';
+import 'package:flutter/cupertino.dart';
+
+class UserDetailsModel extends ChangeNotifier {
+  String _enrNo;
+  String _username;
+  String _token;
+  String _branch;
+  String _hostelName;
+  String _roomNo;
+  String _email;
+  String _contactNo;
+
+  String get enrNo => _enrNo;
+
+  String get username => _username;
+
+  String get token => _token;
+
+  String get branch => _branch;
+
+  String get hostelName => _hostelName;
+
+  String get roomNo => _roomNo;
+
+  String get email => _email;
+
+  String get contactNo => _contactNo;
+
+  UserDetailsModel() {
+    loadUserDetails();
+  }
+
+  /// loads user details from SharedPreferences
+  void loadUserDetails() {
+    getUserDetails().then((details) async {
+      _enrNo = details.getString("enrNo");
+      _username = details.getString("username");
+      _token = details.getString("token");
+      _branch = details.getString("branch");
+      _hostelName = details.getString("hostelName");
+      _roomNo = details.getString("roomNo");
+      _email = details.getString("email");
+      _contactNo = details.getString("contactNo");
+    });
+  }
+}
