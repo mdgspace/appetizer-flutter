9417bb0f3fe015a0781b50e313a83cf3776308cd
am15h <amishgarg123@gmail.com>
2020-01-31 01:00:35 +0530

    rfac: created styles.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:appetizer/styles.dart';
 import 'package:appetizer/ui/menu/home.dart';
 import 'package:appetizer/utils/user_details.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,6 @@ import 'ui/login/login.dart';
 import 'ui/on_boarding/onBoarding.dart';
 
 void main() async {
-
   //await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(ChangeNotifierProvider(
     builder: (context) => MenuModel(),
@@ -28,82 +28,8 @@ void main() async {
         primaryColor: appiYellow,
         accentColor: appiGrey,
         cursorColor: appiYellow,
-        accentTextTheme: TextTheme(
-          //used for authenticating button
-          display1: new TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 19.0,
-              color: Colors.white,
-              fontFamily: "OpenSans"),
-          //used for username in nav drawer
-          display2: TextStyle(
-
-            color: Colors.white,
-            fontSize: 22,
-          ),
-          //used for enrolment no in nav drawer
-          display3: TextStyle(
-            color: appiYellow,
-            fontSize: 15,
-          ),
-          title: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: appiLightGreyText,
-            fontSize: 16,
-          ),
-          subtitle: TextStyle(
-            color: appiLightGreyText,
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        primaryTextTheme: TextTheme(
-          //display1 theme is used for the Login Button
-          display1: new TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 22.0,
-            color: appiYellow,
-            fontFamily: "OpenSans",
-          ),
-          //display2 theme is used for the channel-I button
-          display2: new TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: 14.0,
-              color: Colors.white,
-              fontFamily: "OpenSans"),
-          //display3 theme is used for the help and forgot password button
-          display3: new TextStyle(
-            fontSize: 15.0,
-            color: appiYellow,
-            decoration: TextDecoration.underline,
-            fontFamily: "OpenSans",
-          ),
-          //display4 theme is used for the "showDash function"
-          display4: new TextStyle(
-            fontSize: 15.0,
-            color: appiYellow,
-            fontFamily: "OpenSans",
-          ),
-          //subhead theme is used for the Enrollment No and Password Input field
-          subhead: new TextStyle(
-            fontSize: 17.0,
-            color: appiGreyIcon.withOpacity(0.8),
-            fontFamily: "OpenSans",
-          ),
-          //headline used for forgot password title
-          headline: new TextStyle(
-            fontSize: 24.0,
-            color: appiGreyIcon,
-            fontFamily: "OpenSans",
-          ),
-          //caption used for sub-Text
-          caption: new TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 16.0,
-            color: appiYellow,
-            fontFamily: "OpenSans",
-          ),
-        ),
+        accentTextTheme: accentTextTheme,
+        primaryTextTheme: primaryTextTheme,
       ),
       home: Appetizer(),
     ),
