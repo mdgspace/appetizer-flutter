397bd1cd92e1f8e1c605416da4eebac56f0fc870
am15h <amishgarg123@gmail.com>
2020-02-02 10:33:50 +0530

    feat: dynamic width calculation, pageView setup

--- lib/ui/date_picker/date_picker.dart
+++ lib/ui/date_picker/date_picker.dart
@@ -9,31 +9,63 @@ void main() async {
   //await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(appBar: AppBar(), body: DateRow()),
+    home: Scaffold(appBar: AppBar(), body: DatePicker()),
   ));
+}
+
+class DatePicker extends StatefulWidget{
+  @override
+  _DatePickerState createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+
+  DateTime activeDate;
+
+  @override
+  Widget build(BuildContext context) {
+    final _pageController = PageController(
+      initialPage: 1,
+    );
+    return PageView(
+      controller: _pageController,
+      children: <Widget>[
+        DateRow(),
+        DateRow(),
+        DateRow()
+      ],
+    );
+  }
 }
 
 class DateRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: _currentRowWidgets());
+    final _padding = 20.0;
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(_padding),
+      child: Row(children: _currentRowWidgets(_padding)),
+    );
   }
 
-  List<Widget> _currentRowWidgets() {
-    final list =  _currentRowDates().map((dateTime) {
-      return DateCell(dateTime);
-    }).toList(growable: false);return list;
+  List<Widget> _currentRowWidgets(double _padding) {
+    final list = _currentRowDates().map((dateTime) {
+      return DateCell(dateTime, _padding);
+    }).toList(growable: false);
+    return list;
   }
 
   List<DateTime> _currentRowDates() {
     List<DateTime> _dateList = List(7);
     final _now = DateTime.now();
-    for (int i = _now.weekday-1; i >= 1; i--) {
-      _dateList[i - 1] = _now.subtract(Duration(hours: 24*(_now.weekday-i)));
+    for (int i = _now.weekday - 1; i >= 1; i--) {
+      _dateList[i - 1] =
+          _now.subtract(Duration(hours: 24 * (_now.weekday - i)));
     }
     _dateList[_now.weekday - 1] = _now;
-    for (int i = _now.weekday+1; i <= 7; i++) {
-      _dateList[i - 1] = _now.add(Duration(hours: 24*(i-_now.weekday)));
+    for (int i = _now.weekday + 1; i <= 7; i++) {
+      _dateList[i - 1] = _now.add(Duration(hours: 24 * (i - _now.weekday)));
     }
     print(_dateList);
     return _dateList;
@@ -42,30 +74,41 @@ class DateRow extends StatelessWidget {
 
 class DateCell extends StatelessWidget {
   final DateTime cellDate;
-
-  DateCell(this.cellDate);
+  final double _parentPadding;
+  DateCell(this.cellDate, this._parentPadding);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        Container(
-          width: 24,
-          height: 24,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(),
+
+    final _scrW = MediaQuery.of(context).size.width - (2*_parentPadding);
+    print("ScrW $_scrW");
+    final _padding = 4.0;
+    final _width = (_scrW - (14 * _padding)) / 7;
+    print("W $_width");
+
+    return Container(
+      padding: EdgeInsets.all(_padding),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: _width,
+            height: _width,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(),
+            ),
+            child: Center(child: Text(cellDate.day.toString())),
           ),
-          child: Center(child: Text(cellDate.day.toString())),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
+          SizedBox(height: 4,),
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+            ),
+            child: Text(DateTimeUtils.getWeekDayName(cellDate)[0]),
           ),
-          child: Text(DateTimeUtils.getWeekDayName(cellDate)[0]),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
