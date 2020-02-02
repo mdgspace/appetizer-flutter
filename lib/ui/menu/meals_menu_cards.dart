fd7d0ae2ab94c23d6285fc720c2ea157a3ed002b
am15h <amishgarg123@gmail.com>
2020-02-02 15:30:44 +0530

    feat: OOP for OtherMenu

--- lib/ui/menu/meals_menu_cards.dart
+++ lib/ui/menu/meals_menu_cards.dart
@@ -588,7 +588,7 @@ class OtherMealsMenuCardNew extends StatefulWidget {
 }
 
 class _OtherMealsMenuCardNewState extends State<OtherMealsMenuCardNew> {
-  bool mealSwitchStatus;
+  bool _mealSwitchStatus;
   InheritedData inheritedData;
 
   @override
@@ -602,14 +602,156 @@ class _OtherMealsMenuCardNewState extends State<OtherMealsMenuCardNew> {
   @override
   void initState() {
     super.initState();
-    mealSwitchStatus = widget.meal.mealSwitchStatusBool;
+    _mealSwitchStatus = widget.meal.mealSwitchStatusBool;
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 4.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(12, 4, 12, 4),
+        elevation: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 24.0),
+                          child: Row(
+                            children: <Widget>[
+                              MenuCardUtils.titleAndBhawanNameComponent(widget.meal),
+                            ],
+                          ),
+                        ),
+                      ),
+                      _getSwitchIcon(),
+                    ],
+                  ),
+                  Column(
+                    children: MenuCardUtils.itemWidgetList(widget.meal),
+                  ),
+                ],
+              ),
+            ),
+            MenuCardUtils.dailyItemsComponent(widget.meal, widget.dailyItems),
+          ],
+        ),
+      ),
+    );;
   }
+
+  Widget _getSwitchIcon() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: widget.meal.isSwitchable
+          ? GestureDetector(
+        child: Image.asset(
+          widget.meal.isLeaveToggleOutdated
+              ? "assets/icons/switch_inactive.png"
+              : _mealSwitchStatus
+              ? "assets/icons/switch_active.png"
+              : "assets/icons/switch_crossed_active.png",
+          width: 30,
+          scale: 2,
+        ),
+        onTap: widget.meal.isLeaveToggleOutdated
+            ? null
+            : _mealSwitchStatus
+            ? () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SwitchableMealsScreen(
+                id: widget.meal.id,
+                token: inheritedData.userDetails.token,
+              ),
+            ),
+          );
+        }
+            : widget.meal.switchStatus.status == SwitchStatusEnum.T ||
+            widget.meal.switchStatus.status ==
+                SwitchStatusEnum.F
+            ? () {
+          showDialog(
+            context: context,
+            builder: (BuildContext alertContext) {
+              return AlertDialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                title: new Text(
+                  "Cancel Switch",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                content: new Text(
+                  "Are you sure you want to cancel this switch?",
+                ),
+                actions: <Widget>[
+                  new FlatButton(
+                    onPressed: () {
+                      Navigator.pop(alertContext);
+                    },
+                    child: new Text(
+                      "NO",
+                      style: TextStyle(
+                          color: appiYellow,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  new FlatButton(
+                    child: new Text(
+                      "YES",
+                      style: TextStyle(
+                          color: appiYellow,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () async {
+                      Navigator.pop(alertContext);
+                      showCustomDialog(
+                          context, "Cancelling Switch");
+                      cancelSwitch(
+                          widget.meal.switchStatus.id,
+                          inheritedData
+                              .userDetails.token)
+                          .then((switchCancelResponse) {
+                        Navigator.pop(context);
+                        if (switchCancelResponse) {
+                          Navigator.of(context).popUntil(
+                                  (route) => route.isFirst);
+                          setState(() {
+                            _mealSwitchStatus = true;
+                          });
+                        } else {
+                          Fluttertoast.showToast(
+                              msg:
+                              "Unable to cancel the switch");
+                        }
+                      });
+                    },
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                  ),
+                ],
+              );
+            },
+          );
+        }
+            : null,
+      )
+          : Container(),
+    );
+  }
+
 }
 
 class MenuCardUtils {
