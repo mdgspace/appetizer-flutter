3f23a9327d4e4a81da28255a628a4be0c11ae54d
am15h <amishgarg123@gmail.com>
2020-02-21 14:46:07 +0530

    ptch: threshold time changed from 8 to 12hrs

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 var menuScaffoldKey = new GlobalKey<ScaffoldState>();
 bool isCheckedOut = false;
 bool isCheckedLoading = false;
-Duration outdatedTime = Duration(hours: 8);
+Duration outdatedTime = Duration(hours: 12);
 String url = "https://api.mess.vishwas.rocks";
