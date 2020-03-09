008c44da11564a9a3d011453a0cfff13cddc81b1
Amish Garg <amishgarg123@gmail.com>
2020-03-09 16:31:04 +0530

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,7 +4,7 @@ var menuScaffoldKey = new GlobalKey<ScaffoldState>();
 bool isCheckedOut = false;
 bool isCheckedLoading = false;
 Duration outdatedTime = Duration(hours: 12);
-String url = "http://appetizer-mdg.herokuapp.com";
-//String url = "https://api.mess.vishwas.rocks";
+//String url = "http://appetizer-mdg.herokuapp.com";
+String url = "https://api.mess.vishwas.rocks";
 
 enum TokenStatus { INVALID_TOKEN, OK }
