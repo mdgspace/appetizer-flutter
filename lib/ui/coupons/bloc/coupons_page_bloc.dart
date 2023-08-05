8687ee5d65bcfdcb303e269c8c9a92b6c8b95cc8
NanoNish <nishkarsh1206@gmail.com>
2023-08-06 02:24:30 +0530

    [WIP] feat: coupons page (no constraints, banner)

--- lib/ui/coupons/bloc/coupons_page_bloc.dart
+++ lib/ui/coupons/bloc/coupons_page_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:appetizer_revamp_parts/models/coupon/coupon.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -5,9 +6,25 @@ part 'coupons_page_event.dart';
 part 'coupons_page_state.dart';
 
 class CouponsPageBloc extends Bloc<CouponsPageEvent, CouponsPageState> {
-  CouponsPageBloc() : super(CouponsPageInitial()) {
-    on<CouponsPageEvent>((event, emit) {
-      // TODO: implement event handler
+  CouponsPageBloc() : super(const CouponsPageInitialState()) {
+    on<CouponsPageFetchEvent>(
+        (CouponsPageFetchEvent event, Emitter<CouponsPageState> emit) {
+      // TODO: implement repository call
+      bool submissionSuccessful = true;
+      List<Coupon> coupons = [];
+      if (submissionSuccessful) {
+        emit(
+          CouponsPageFetchedState(
+            coupons: coupons,
+          ),
+        );
+      } else {
+        emit(
+          CouponsPageFailedState(
+            coupons: event.coupons,
+          ),
+        );
+      }
     });
   }
 }
--- lib/ui/coupons/bloc/coupons_page_state.dart
+++ lib/ui/coupons/bloc/coupons_page_state.dart
@@ -2,9 +2,36 @@ part of 'coupons_page_bloc.dart';
 
 abstract class CouponsPageState extends Equatable {
   const CouponsPageState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class CouponsPageInitial extends CouponsPageState {}
+class CouponsPageInitialState extends CouponsPageState {
+  const CouponsPageInitialState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CouponsPageFetchedState extends CouponsPageState {
+  const CouponsPageFetchedState({
+    required this.coupons,
+  });
+
+  final List<Coupon> coupons;
+
+  @override
+  List<Object> get props => [coupons];
+}
+
+class CouponsPageFailedState extends CouponsPageState {
+  const CouponsPageFailedState({
+    required this.coupons,
+  });
+
+  final List<Coupon> coupons;
+
+  @override
+  List<Object> get props => [coupons];
+}
