ca3e6540047a9c7285d83ab44a73416daef449ca
NanoNish <nishkarsh1206@gmail.com>
2023-08-06 01:36:59 +0530

    feat: complete feedback page (except button)

--- lib/ui/feedback/bloc/feedback_page_bloc.dart
+++ lib/ui/feedback/bloc/feedback_page_bloc.dart
@@ -7,12 +7,8 @@ part 'feedback_page_state.dart';
 class FeedbackPageBloc extends Bloc<FeedbackPageEvent, FeedbackPageState> {
   FeedbackPageBloc()
       : super(
-          const FeedbackPageState(
-            rating1: 0,
-            rating2: 0,
-            rating3: 0,
-            rating4: 0,
-            rating5: 0,
+          FeedbackPageState(
+            rating: List<int>.filled(5, 0),
             description: '',
             submitted: false,
             error: false,
@@ -21,34 +17,25 @@ class FeedbackPageBloc extends Bloc<FeedbackPageEvent, FeedbackPageState> {
     on<FeedbackPageSubmitEvent>(
         (FeedbackPageSubmitEvent event, Emitter<FeedbackPageState> emit) {
       // TODO: implement repository call
-      bool submissionSuccessful = true;
-      if (submissionSuccessful) {
+      bool _submissionSuccessful = true;
+      if (_submissionSuccessful) {
         emit(
           FeedbackPageState(
-            rating1: event.rating1,
-            rating2: event.rating2,
-            rating3: event.rating3,
-            rating4: event.rating4,
-            rating5: event.rating5,
+            rating: event.rating,
             description: event.description,
             submitted: true,
             error: false,
           ),
         );
-      }
-      else {
+      } else {
         emit(
           FeedbackPageState(
-            rating1: event.rating1,
-            rating2: event.rating2,
-            rating3: event.rating3,
-            rating4: event.rating4,
-            rating5: event.rating5,
+            rating: event.rating,
             description: event.description,
             submitted: false,
             error: true,
           ),
-        );  
+        );
       }
     });
     on<FeedbackPageDescriptionChangedEvent>(
@@ -57,11 +44,7 @@ class FeedbackPageBloc extends Bloc<FeedbackPageEvent, FeedbackPageState> {
       if (event.description != state.description) {
         emit(
           FeedbackPageState(
-            rating1: state.rating1,
-            rating2: state.rating2,
-            rating3: state.rating3,
-            rating4: state.rating4,
-            rating5: state.rating5,
+            rating: state.rating,
             description: event.description,
             submitted: state.submitted,
             error: state.error,
--- lib/ui/feedback/bloc/feedback_page_event.dart
+++ lib/ui/feedback/bloc/feedback_page_event.dart
@@ -9,22 +9,14 @@ abstract class FeedbackPageEvent extends Equatable {
 
 class FeedbackPageSubmitEvent extends FeedbackPageEvent {
   const FeedbackPageSubmitEvent({
-    required this.rating1,
-    required this.rating2,
-    required this.rating3,
-    required this.rating4,
-    required this.rating5,
+    required this.rating,
     required this.description,
   });
-  final int rating1;
-  final int rating2;
-  final int rating3;
-  final int rating4;
-  final int rating5;
+  final List<int> rating;
   final String description;
 
   @override
-  List<Object> get props => [rating1, rating2, rating3, rating4, rating5, description];
+  List<Object> get props => [rating, description];
 }
 
 class FeedbackPageDescriptionChangedEvent extends FeedbackPageEvent {
