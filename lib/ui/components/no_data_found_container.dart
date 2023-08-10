06b97114208c586c5806d501da4630f697df14ae
NanoNish <nishkarsh1206@gmail.com>
2023-08-11 02:35:46 +0530

    feat: add placeholder widget when no data present

---
+++ lib/ui/components/no_data_found_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NoDataFoundContainer extends StatelessWidget {
+  const NoDataFoundContainer({
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(top: 150),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 150),
+            height: 178,
+            width: 186,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/assets/images/no_data_image.png'),
+              ),
+            ),
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Color(0xFF111111),
+              fontSize: 18,
+              fontFamily: 'Noto Sans',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
