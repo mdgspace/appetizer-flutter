3bf59147f3edf3800c123bbf1466b5ee2fa19c87
Manan Garg <manantechy@gmail.com>
2023-08-17 01:31:55 +0530

    ptch: fix divider on menu title bar

--- lib/ui/YourWeekMenu/components/title_bar.dart
+++ lib/ui/YourWeekMenu/components/title_bar.dart
@@ -18,42 +18,31 @@ class TitleBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              Text(dayName, style: AppTheme.headline1),
-              SizedBox(
-                width: 8,
-              ),
-              Transform(
-                transform: Matrix4.identity()
-                  ..translate(0.0, 0.0)
-                  ..rotateZ(1.57),
-                child: Container(
-                  width: 24,
-                  decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        width: 0.50,
-                        strokeAlign: BorderSide.strokeAlignCenter,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
+          IntrinsicHeight(
+            child: Row(
+              children: [
+                Text(dayName, style: AppTheme.headline1),
+                SizedBox(
+                  width: 8,
                 ),
-              ),
-              Text(monthAndYear,
-                  style: AppTheme.headline1.copyWith(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  )),
-              SizedBox(
-                width: 6,
-              ),
-              Icon(
-                Icons.calendar_today_outlined,
-                color: AppTheme.white,
-              ),
-            ],
+                VerticalDivider(
+                  thickness: 1.5,
+                  color: AppTheme.white,
+                ),
+                Text(monthAndYear,
+                    style: AppTheme.headline1.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    )),
+                SizedBox(
+                  width: 6,
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  color: AppTheme.white,
+                ),
+              ],
+            ),
           ),
           SvgPicture.asset('assets/images/icons/Bell.svg'),
         ],
