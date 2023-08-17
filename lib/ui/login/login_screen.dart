7fbd732ba89a09b11b5194b5389fa4694a95624a
NanoNish <nishkarsh1206@gmail.com>
2023-08-17 18:46:27 +0530

    patch: fix logo size

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -19,36 +19,114 @@ class _LoginScreenState extends State<LoginScreen> {
       create: (context) => LoginBloc(),
       child: Scaffold(
           resizeToAvoidBottomInset: false,
-          body: Column(
-            children: <Widget>[
-              Image.asset('assets/images/login.png'),
-              const SizedBox(height: 20),
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  scale: 1.5,
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/images/login.png'),
+                const SizedBox(height: 20),
+                Container(
+                  width: 168,
+                  height: 63,
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    scale: 1.5,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: BlocConsumer<LoginBloc, LoginState>(
-                  listener: (context, state) {
-                    if (state is EnterPassword) {
-                      _controller.clear();
-                    }
-                  },
-                  builder: (context, state) {
-                    if (state is Loading) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (state is CreatePassword) {
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: BlocConsumer<LoginBloc, LoginState>(
+                    listener: (context, state) {
+                      if (state is EnterPassword) {
+                        _controller.clear();
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is Loading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      if (state is CreatePassword) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Set Password',
+                              style: GoogleFonts.notoSans(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            TextField(
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                hintText: 'Create Password',
+                                hintStyle: GoogleFonts.lato(
+                                  fontSize: 12,
+                                  color: const Color(0xFF111111),
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: const Color(0xFF111111)
+                                          .withOpacity(0.25)),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            TextField(
+                              controller: _controller,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Confirm Password',
+                                hintStyle: GoogleFonts.lato(
+                                  fontSize: 12,
+                                  color: const Color(0xFF111111),
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: const Color(0xFF111111)
+                                          .withOpacity(0.25)),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                contentPadding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                              child: Text(
+                                state.error ?? '',
+                                style: GoogleFonts.lato(
+                                  fontSize: 10,
+                                  color: const Color(0xFF2F2F2F),
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Center(
+                                child: LoginButton(
+                                    text: 'Login',
+                                    onPressed: () {
+                                      context
+                                          .read<LoginBloc>()
+                                          .add(LoginPressed(
+                                            _controller.text,
+                                          ));
+                                    })),
+                          ],
+                        );
+                      }
                       return Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Set Password',
+                            'Login/SignUp',
                             style: GoogleFonts.notoSans(
                               fontSize: 18,
                               fontWeight: FontWeight.w600,
@@ -57,13 +135,31 @@ class _LoginScreenState extends State<LoginScreen> {
                           const SizedBox(height: 20),
                           TextField(
                             controller: _controller,
+                            obscureText: state is EnterPassword
+                                ? !state.showPassword
+                                : false,
                             decoration: InputDecoration(
-                              hintText: 'Create Password',
+                              hintText: state is EnterPassword
+                                  ? "Password"
+                                  : 'Enrollment No.',
                               hintStyle: GoogleFonts.lato(
                                 fontSize: 12,
                                 color: const Color(0xFF111111),
                                 fontWeight: FontWeight.w600,
                               ),
+                              suffixIcon: state is EnterPassword
+                                  ? IconButton(
+                                      onPressed: () {
+                                        BlocProvider.of<LoginBloc>(context).add(
+                                          ShowPasswordPressed(),
+                                        );
+                                      },
+                                      icon: state.showPassword
+                                          ? const Icon(Icons.visibility,
+                                              color: Color(0xFF757575))
+                                          : const Icon(Icons.visibility_off,
+                                              color: Color(0xFF757575)))
+                                  : null,
                               border: OutlineInputBorder(
                                 borderSide: BorderSide(
                                     color: const Color(0xFF111111)
@@ -74,165 +170,76 @@ class _LoginScreenState extends State<LoginScreen> {
                                   const EdgeInsets.symmetric(horizontal: 20),
                             ),
                           ),
-                          const SizedBox(height: 10),
-                          TextField(
-                            controller: _controller,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: 'Confirm Password',
-                              hintStyle: GoogleFonts.lato(
-                                fontSize: 12,
-                                color: const Color(0xFF111111),
-                                fontWeight: FontWeight.w600,
-                              ),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: const Color(0xFF111111)
-                                        .withOpacity(0.25)),
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              contentPadding:
-                                  const EdgeInsets.symmetric(horizontal: 20),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 30,
-                            child: Text(
-                              state.error ?? '',
-                              style: GoogleFonts.lato(
-                                fontSize: 10,
-                                color: const Color(0xFF2F2F2F),
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          Center(
-                              child: LoginButton(
-                                  text: 'Login',
-                                  onPressed: () {
-                                    context.read<LoginBloc>().add(LoginPressed(
-                                          _controller.text,
-                                        ));
-                                  })),
-                        ],
-                      );
-                    }
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Login/SignUp',
-                          style: GoogleFonts.notoSans(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        TextField(
-                          controller: _controller,
-                          obscureText: state is EnterPassword
-                              ? !state.showPassword
-                              : false,
-                          decoration: InputDecoration(
-                            hintText: state is EnterPassword
-                                ? "Password"
-                                : 'Enrollment No.',
-                            hintStyle: GoogleFonts.lato(
-                              fontSize: 12,
-                              color: const Color(0xFF111111),
-                              fontWeight: FontWeight.w600,
-                            ),
-                            suffixIcon: state is EnterPassword
-                                ? IconButton(
-                                    onPressed: () {
-                                      BlocProvider.of<LoginBloc>(context).add(
-                                        ShowPasswordPressed(),
-                                      );
-                                    },
-                                    icon: state.showPassword
-                                        ? const Icon(Icons.visibility,
-                                            color: Color(0xFF757575))
-                                        : const Icon(Icons.visibility_off,
-                                            color: Color(0xFF757575)))
-                                : null,
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                  color: const Color(0xFF111111)
-                                      .withOpacity(0.25)),
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            contentPadding:
-                                const EdgeInsets.symmetric(horizontal: 20),
-                          ),
-                        ),
-                        state is EnterPassword
-                            ? SizedBox(
-                                height: 30,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      state.error ?? '',
-                                      style: GoogleFonts.lato(
-                                        fontSize: 10,
-                                        color: const Color(0xFF2F2F2F),
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    TextButton(
-                                      onPressed: () {},
-                                      child: Text(
-                                        'Forgot Password?',
+                          state is EnterPassword
+                              ? SizedBox(
+                                  height: 30,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        state.error ?? '',
                                         style: GoogleFonts.lato(
                                           fontSize: 10,
                                           color: const Color(0xFF2F2F2F),
                                           fontWeight: FontWeight.w400,
-                                          textStyle: const TextStyle(
-                                            decoration:
-                                                TextDecoration.underline,
+                                        ),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Forgot Password?',
+                                          style: GoogleFonts.lato(
+                                            fontSize: 10,
+                                            color: const Color(0xFF2F2F2F),
+                                            fontWeight: FontWeight.w400,
+                                            textStyle: const TextStyle(
+                                              decoration:
+                                                  TextDecoration.underline,
+                                            ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            : const SizedBox(height: 20),
-                        Center(
-                            child: LoginButton(
-                                text: state is EnterPassword ? 'Login' : 'Next',
-                                onPressed: () {
-                                  state is EnterPassword
-                                      ? context
-                                          .read<LoginBloc>()
-                                          .add(LoginPressed(
-                                            _controller.text,
-                                          ))
-                                      : context
-                                          .read<LoginBloc>()
-                                          .add(NextPressed(_controller.text));
-                                })),
-                      ],
-                    );
-                  },
+                                    ],
+                                  ),
+                                )
+                              : const SizedBox(height: 20),
+                          Center(
+                              child: LoginButton(
+                                  text:
+                                      state is EnterPassword ? 'Login' : 'Next',
+                                  onPressed: () {
+                                    state is EnterPassword
+                                        ? context
+                                            .read<LoginBloc>()
+                                            .add(LoginPressed(
+                                              _controller.text,
+                                            ))
+                                        : context
+                                            .read<LoginBloc>()
+                                            .add(NextPressed(_controller.text));
+                                  })),
+                        ],
+                      );
+                    },
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Raise a Query',
-                  style: GoogleFonts.inter(
-                    fontSize: 12,
-                    color: const Color(0xFF008BFF),
-                    fontWeight: FontWeight.w400,
-                    textStyle: const TextStyle(
-                      decoration: TextDecoration.underline,
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Raise a Query',
+                    style: GoogleFonts.inter(
+                      fontSize: 12,
+                      color: const Color(0xFF008BFF),
+                      fontWeight: FontWeight.w400,
+                      textStyle: const TextStyle(
+                        decoration: TextDecoration.underline,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )),
     );
   }
