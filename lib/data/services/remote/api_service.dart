b4e9013be93bea1ee369eb7a4a80fa7a2994e05f
NanoNish <nishkarsh1206@gmail.com>
2023-08-24 18:49:09 +0530

    feat: add coupon and feedback to api service

--- lib/data/services/remote/api_service.dart
+++ lib/data/services/remote/api_service.dart
@@ -1,6 +1,10 @@
 import 'package:appetizer/data/constants/api_endpoints.dart';
 import 'package:appetizer/domain/models/appetizer_version.dart';
-import 'package:dio/dio.dart';
+import 'package:appetizer/domain/models/coupon/coupon.dart';
+import 'package:appetizer/domain/models/feedback/appetizer_feedback.dart';
+import 'package:appetizer/domain/models/feedback/feedback_response.dart';
+import 'package:appetizer/domain/models/menu/week_menu.dart';
+import 'package:dio/dio.dart' hide Headers;
 import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
@@ -14,4 +18,35 @@ abstract class ApiService {
     @Path("platform") String platform,
     @Path("versionNumber") String versionNumber,
   );
+
+  // TODO: add token to all headers
+  // TODO: find a way to remove maps
+
+  // Coupon API
+
+  @POST(ApiEnpoints.coupon)
+  Future<CouponStatus> applyForCoupon(
+    @Body() Map<String, dynamic> map,
+  );
+
+  @PATCH(ApiEnpoints.couponWithId)
+  Future<CouponStatus> cancelCoupon(
+    @Path("couponId") String couponId,
+    @Body() Map<String, dynamic> map,
+  );
+
+  // Feedback API
+
+  @GET(ApiEnpoints.submittedFeedback)
+  Future<List<AppetizerFeedback>> submittedFeedbacks();
+
+  @GET(ApiEnpoints.responseOfFeedback)
+  Future<List<FeedbackResponse>> responseOfFeedbacks();
+
+  @POST(ApiEnpoints.newFeedback)
+  Future<AppetizerFeedback> newFeedback(
+    @Body() Map<String, dynamic> map,
+  );
+
+  
 }
