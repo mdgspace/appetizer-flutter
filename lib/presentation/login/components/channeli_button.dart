481708a787e9686d3e46fd74262425a07240035f
NanoNish <nishkarsh1206@gmail.com>
2023-09-08 01:01:08 +0530

    chore: remove extra imports

--- lib/presentation/login/components/channeli_button.dart
+++ lib/presentation/login/components/channeli_button.dart
@@ -1,6 +1,5 @@
 import 'package:appetizer/data/core/router/intrinsic_router/intrinsic_router.dart';
 import 'package:appetizer/presentation/login/bloc/login_bloc.dart';
-import 'package:appetizer/utils/app_extensions/app_extensions.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,8 +19,7 @@ class ChanneliButton extends StatelessWidget {
           var code = await context.router.push(OAuthWebRoute());
           if (code != null) {
             context.read<LoginBloc>().add(NewUserSignUp(code: code.toString()));
-          }
-          else {
+          } else {
             // TODO: throw error
           }
         },
