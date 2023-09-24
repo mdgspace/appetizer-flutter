5a037854102cbd8b92cbbccf734fca3932f77593
NanoNish <nishkarsh1206@gmail.com>
2023-09-24 06:03:43 +0530

    feat: add date picker to menu view

--- lib/presentation/week_menu/components/title_bar.dart
+++ lib/presentation/week_menu/components/title_bar.dart
@@ -1,8 +1,12 @@
 import 'package:appetizer/app_theme.dart';
 import 'package:appetizer/data/core/router/intrinsic_router/intrinsic_router.gr.dart';
 import 'package:appetizer/data/core/theme/dimensional/dimensional.dart';
+import 'package:appetizer/presentation/week_menu/bloc/week_menu_bloc.dart';
+import 'package:appetizer/presentation/week_menu/components/DayDateBar/bloc/day_date_bar_bloc.dart';
+import 'package:appetizer/utils/date_time_utils.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class TitleBar extends StatelessWidget {
@@ -38,9 +42,44 @@ class TitleBar extends StatelessWidget {
                   ),
                 ),
                 6.toHorizontalSizedBox,
-                const Icon(
-                  Icons.calendar_today_outlined,
-                  color: AppTheme.white,
+                GestureDetector(
+                  onTap: () async {
+                    DateTime? newDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2024),
+                      builder: (BuildContext context, Widget? child) {
+                        return Theme(
+                          data: ThemeData.light().copyWith(
+                            colorScheme: const ColorScheme.light(
+                              primary: AppTheme.primary,
+                              onPrimary: AppTheme.white,
+                              surface: AppTheme.white,
+                              onSurface: AppTheme.black1e,
+                            ),
+                            dialogBackgroundColor: Colors.white,
+                          ),
+                          child: child ?? const SizedBox.shrink(),
+                        );
+                      },
+                    );
+                    if (newDate != null && context.mounted) {
+                      context.read<DayDateBarBloc>().add(
+                            DayDateBarChangedEvent(
+                              newDate: newDate,
+                            ),
+                          );
+                      context.read<WeekMenuBlocBloc>().add(DateChangeEvent(
+                            dayIndex: newDate.weekday - 1,
+                            weekId: DateTimeUtils.getWeekNumber(newDate),
+                          ));
+                    }
+                  },
+                  child: const Icon(
+                    Icons.calendar_today_outlined,
+                    color: AppTheme.white,
+                  ),
                 ),
               ],
             ),
