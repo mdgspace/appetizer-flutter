c9ce9600e6b6db6cad04abeb45f631ce268a2af8
NanoNish <nishkarsh1206@gmail.com>
2023-09-29 01:18:13 +0530

    fix: enforce portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ import 'package:appetizer/presentation/app/app.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await LocalStorageService.init();
 
